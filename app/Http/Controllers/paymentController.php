e40e018ad6d42f46a772f4c036c9c241076d5082
GKokonya <georgekokonya@gmail.com>
2023-05-07 01:51:19 +0300

    5th commit

--- app/Http/Controllers/paymentController.php
+++ app/Http/Controllers/paymentController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Payment;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
@@ -12,7 +11,7 @@ class PaymentController extends Controller
      */
     public function index()
     {
-        
+        //
         return view( 'payments.index' , [ 'payments' => Payment::paginate(10) ]) ;
     }
 
@@ -35,7 +34,7 @@ class PaymentController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Payment $payment)
+    public function show(string $id)
     {
         //
     }
@@ -43,7 +42,7 @@ class PaymentController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Payment $payment)
+    public function edit(string $id)
     {
         //
     }
@@ -51,7 +50,7 @@ class PaymentController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Payment $payment)
+    public function update(Request $request, string $id)
     {
         //
     }
@@ -59,7 +58,7 @@ class PaymentController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Payment $payment)
+    public function destroy(string $id)
     {
         //
     }
